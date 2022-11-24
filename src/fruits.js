0e19b3861af6691eb1315d19b0e296aaa3a80df3
anikettikkal <tikkalaniket2019@gmail.com>
2022-11-24 06:38:39 +0000

    added changes

---
+++ src/fruits.js
@@ -0,0 +1,25 @@
+import "./fruits.css"
+
+export function Apple() {
+    return (
+        <>
+            <h1 className="container">Apple🍎</h1>
+        </>
+    )
+}
+
+export function Mango() {
+    return (
+        <>
+            <h1 className="container">Mango🍋</h1>
+        </>
+    )
+}
+
+export default function MyFruits(){
+    return (
+        <>
+            <h1 className="container"> Mango🍋 & Apple🍎 </h1>
+        </>
+    )
+}
